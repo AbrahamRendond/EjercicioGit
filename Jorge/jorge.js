ed43bd2c84b42b8dc4f1ec41e4bc74259489833b
AbrahamRendond <aranita209@gmail.com>
2022-03-30 14:25:15 -0600

    Prueba

---
+++ Jorge/jorge.js
@@ -0,0 +1,17 @@
+// FIXME: No puede ser Orozco paso por aqui y desordeno el codigo necesito tu ayuda
+function dividir(a, b) {
+    const c = a /---/ b;
+    return c;
+  }
+
+  function mostrarMensaje() {
+
+  // TODO: Parsear los valores a números.
+
+    const a = prompt('Introduce el número 1.');
+    const b = prompt('Introduce el número 2.');
+
+    const res = alert(dividir(a, b));
+  }
+
+  mostrarMensaje();
---
+++ Mancebo/Mancebo.js
@@ -0,0 +1,17 @@
+// FIXME: No puede ser Orozco paso por aqui y desordeno el codigo necesito tu ayuda
+function resta(a, b) {
+    const c = a ----- b;
+    return c;
+  }
+
+  function mostrarMensaje() {
+
+  // TODO: Parsear los valores a números.
+
+    const a = prompt('Introduce el número 1.');
+    const b = prompt('Introduce el número 2.');
+
+    const res = alert(resta(a, b));
+  }
+
+  mostrarMensaje();
